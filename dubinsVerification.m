c07752866e37682e86335715825add98688f6fad
draxnor <medyk.pawel@gmail.com>
2020-02-03 12:15:23 +0100

    Dodanie miniprojektu

--- dubinsVerification.m
+++ dubinsVerification.m
@@ -2,7 +2,7 @@ close all;
 
 figure(1); hold on; grid on;
 q1 = VehicleState(5,3,pi/2.6);
-q2 = VehicleState(2,4,pi/3.6);
+q2 = VehicleState(4,4,pi/3.6);
 % q1 = VehicleState(1,3,pi/2);
 % q2 = VehicleState(1,4,pi/2);
 
@@ -15,25 +15,25 @@ q2.LeftCirclePlot();
 q2.StatePlot();
 
 try
-    RSL_Path(q1,q2);
-    LSR_Path(q1,q2);
-    RSR_Path(q1,q2);
-    LSL_Path(q1,q2);
-    RLR_Path(q1,q2);
-    LRL_Path(q1,q2);
+%     RSL_Path(q1,q2);
+%     LSR_Path(q1,q2);
+     RSR_Path(q1,q2);
+     LSL_Path(q1,q2);
+%     RLR_Path(q1,q2);
+ %   LRL_Path(q1,q2);
 catch ME
     msg=ME.message
 end
-
-
-q3 = VehicleState(1,1,pi);
-q3.StatePlot();
-
-try
-    RSL_Path(q2,q3);
-    LSR_Path(q2,q3)
-catch ME
-    msg=ME.message
-end
+% 
+% 
+% q3 = VehicleState(1,1,pi);
+% q3.StatePlot();
+% 
+% try
+%     RSL_Path(q2,q3);
+%     LSR_Path(q2,q3)
+% catch ME
+%     msg=ME.message
+% end
     
 axis equal
